90b16efa294bfaf74231717b37dff6d3522e993a
Harystolho <haristolhogamer@gmail.com>
2019-02-20 20:14:31 -0300

    Main file for Spring

--- src/main/java/com/harystolho/Main.java
+++ src/main/java/com/harystolho/Main.java
@@ -1,10 +1,14 @@
 package com.harystolho;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.support.AbstractApplicationContext;
+
+@SpringBootApplication
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		AbstractApplicationContext ctx = (AbstractApplicationContext) SpringApplication.run(Main.class, args);
 	}
 
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,4 +7,9 @@
  */
 module website_api {
 	exports com.harystolho;
+
+	requires spring.context;
+	requires spring.boot;
+	requires spring.boot.autoconfigure;
+	requires spring.core;
 }
