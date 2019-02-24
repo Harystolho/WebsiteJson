65db95294d493cc19d2af8e4a2db258b421104b8
Harystolho <haristolhogamer@gmail.com>
2019-02-24 16:14:23 -0300

    Display modules from category when it's clicked

--- src/main/webapp/js/discover.js
+++ src/main/webapp/js/discover.js
@@ -10,12 +10,37 @@ let Discover = (function ($) {
      * Displays the modules from the corresponding category
      * @param categoryID one from CATEGORY_IDS
      */
-    functions.displayCategory = function(categoryID){
+    functions.displayCategory = function (categoryID) {
+        getModulesFromCategory(categoryID, (modules) => {
+            let container = $("#category-display");
+            container.children().remove();
 
+            modules.forEach((mod) => {
+                let moduleElement = createModuleElement(mod);
+                container.append(moduleElement);
+            });
+        });
     };
 
-    function getModulesFromCategory(categoryID){
+    function getModulesFromCategory(categoryID, cb) {
+        $.get("/api/category", {category: categoryID}).done((data) => {
+            cb(data.data);
+        });
+    }
 
+    /**
+     * Creates HTMLElement from Module object
+     * @param mod
+     * @return {*|HTMLElement}
+     */
+    function createModuleElement(mod) {
+        let el = `
+        <div>
+            <div>${mod.name}</div>                   
+        </div>
+        `;
+
+        return $(el);
     }
 
     return functions;
